b8421c020d9e275c23518a7a8ace626487951f9c
sensei100 <jenanton01@gmail.com>
2017-03-25 15:00:41 -0400

    twitter share working

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 getQuote();
 
+document.getElementById("tweet-btn").addEventListener("click", tweetQuote);
+
 function getQuote() { 
   $("#quote").click(function(){
   $.ajax({
@@ -12,8 +14,14 @@ function getQuote() {
       },
     error: function(err) { alert(err); },
     beforeSend: function(xhr) {
-      xhr.setRequestHeader("X-Mashape-Authorization",   "bTBF7sHUgOmshLzhmPB4gv4pWealp1zPZBfjsniFXuP9PuGfzx");
-      } 
+      xhr.setRequestHeader("X-Mashape-Authorization",   "bTBF7sHUgOmshLzhmPB4gv4pWealp1zPZBfjsniFXuP9PuGfzx");} 
     });  
   });
 }
+
+function tweetQuote() {
+  var phrase = document.getElementById("quote-display").innerHTML;
+    var encodedPhrase = encodeURIComponent(phrase);
+    var url = "https://twitter.com/intent/tweet?text=" + encodedPhrase;
+    window.open(url, 'tweetPage', 'resizable, scrollbars, height=550, width=500');
+  };
